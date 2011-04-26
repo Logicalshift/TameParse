8983491fe400f9b4f9f84fb2301aa7c412cccadd
Andrew Hunter <andrew@logicalshift.co.uk>
2011-04-26 15:48:27 +0000

    Hmm, I think this is all that's required to get the deduplicator working
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%40813

--- Dfa/remapped_symbol_map.cpp
+++ Dfa/remapped_symbol_map.cpp
@@ -15,6 +15,9 @@
 using namespace std;
 using namespace dfa;
 
+/// \brief Empty symbol set
+const remapped_symbol_map::new_symbols remapped_symbol_map::s_NoSymbols;
+
 /// \brief Generates an empty remapped symbol map
 remapped_symbol_map::remapped_symbol_map() {
 }
@@ -24,8 +27,50 @@ remapped_symbol_map::remapped_symbol_map(const remapped_symbol_map& copyFrom)
 : m_OldToNew(copyFrom.m_OldToNew) {
 }
 
+/// \brief Finds or adds an identifier for the specified symbol, adding the specified set of new symbols as the new symbols
+int remapped_symbol_map::identifier_for_symbols(const symbol_set& symbols, const new_symbols& newSymbols) {
+    // Get the set ID for these symbols
+    int setId = symbol_map::identifier_for_symbols(symbols);
+    
+    // Try to look up the existing set
+    old_to_new_map::iterator existing = m_OldToNew.find(setId);
+    
+    if (existing == m_OldToNew.end()) {
+        // Just add these symbols
+        m_OldToNew[setId] = newSymbols;
+    } else {
+        // Merge the sets
+        for (new_symbols::iterator it = newSymbols.begin(); it != newSymbols.end(); it++) {
+            existing->second.insert(*it);
+        }
+    }
+    
+    // Return this as the result
+    return setId;
+}
+
+/// \brief Range of symbols
+typedef range<int> symbol_range;
+
 /// \brief Type mapping symbol ranges to the sets that contain them
-typedef map<range<int>, set<int>, range<int>::sort_by_lower > sets_for_range;
+typedef map<symbol_range, set<int>, symbol_range::sort_by_lower > sets_for_range;
+
+/// \brief Splits a range that exists in sets into two.
+///
+/// We end up with two ranges: lower..splitPoint and splitPoint..upper
+/// Returns an iterator for the start of the new ranges
+static sets_for_range::iterator split(sets_for_range& sets, symbol_range original, int splitPoint) {
+    // Get the values for this range
+    set<int> values = sets[original];
+    
+    // Remove the original range
+    sets.erase(original);
+    
+    // Create the split ranges
+    sets[symbol_range(splitPoint, original.upper())] = values;
+    sets_for_range::iterator result = sets.insert(sets_for_range::value_type(symbol_range(original.lower(), splitPoint), values)).first;
+    return result;
+}
 
 /// \brief Adds the ranges in a particular symbol set 
 static void add_set(sets_for_range& sets, const symbol_set& symbols, int identifier) {
@@ -34,9 +79,72 @@ static void add_set(sets_for_range& sets, const symbol_set& symbols, int identif
         // Find the first item in the set whose lower bound is >= this range
         sets_for_range::iterator firstGreaterThan = sets.lower_bound(*range);
         
-        // If this isn't at the beginning, then it's possible we overlap the preceeding sets
+        // If this isn't at the beginning, then it's possible that this set overlaps the preceeding set
         if (firstGreaterThan != sets.begin()) {
+            // Get the previous set
+            sets_for_range::iterator preceeding = firstGreaterThan;
+            preceeding--;
             
+            // See if there's an overlap
+            if (preceeding->first.upper() >= range->lower()) {
+                // Start from the preceeding set if they do
+                firstGreaterThan = preceeding;
+            }
+        }
+        
+        // If we're at the end of the list here, then this is an entirely new range
+        // This is also a new range if the item at firstGreaterThan doesn't overlap the range
+        // Finally, we can just add to the set for a particular range if we get an exact match
+        if (firstGreaterThan == sets.end() || !firstGreaterThan->first.overlaps(*range) || firstGreaterThan->first == *range) {
+            // Just add this range
+            sets[*range].insert(identifier);
+            continue;
+        }
+
+        // We've got some overlap, but it's not exact
+        symbol_range    remaining = *range;                 // Range of characters remaining to merge in
+        set<int>        justUs;                             // Set containing only the new identifier
+        
+        justUs.insert(identifier);
+
+        while (remaining.upper() != remaining.lower()) {
+            // If there's a gap between the remaining set and the current position, then fill it in
+            if (firstGreaterThan == sets.end() || firstGreaterThan->first.lower() > remaining.lower()) {
+                int newUpper = firstGreaterThan->first.lower();
+                
+                // Insert a value to fill the gap
+                firstGreaterThan = sets.insert(firstGreaterThan, sets_for_range::value_type(symbol_range(remaining.lower(), newUpper), justUs));
+                
+                // Adjust the remaining set
+                remaining = symbol_range(newUpper, remaining.upper());
+                
+                // Want the value after the one we just inserted
+                firstGreaterThan++;
+                
+                // Start again
+                continue;
+            }
+            
+            // If there's a gap between the lowest point in the range we're currently splitting and the current range, then split there
+            if (firstGreaterThan->first.lower() < remaining.lower()) {
+                // Split the ranges and move so that the lowest point matches
+                firstGreaterThan = split(sets, firstGreaterThan->first, remaining.lower());
+                firstGreaterThan++;
+            }
+            
+            // If the current range ends beyond the end of the remaining set, then split it
+            if (firstGreaterThan->first.upper() > remaining.upper()) {
+                firstGreaterThan = split(sets, firstGreaterThan->first, remaining.upper());
+            }
+            
+            // Current range should overlap the range specified by firstGreaterThan: add this identifier
+            firstGreaterThan->second.insert(identifier);
+            
+            // Adjust the remaining set
+            remaining = symbol_range(firstGreaterThan->first.upper(), remaining.upper());
+            
+            // Move on
+            firstGreaterThan++;
         }
     }
 }
@@ -55,5 +163,14 @@ remapped_symbol_map* remapped_symbol_map::deduplicate(const symbol_map& source) 
         add_set(setsContainingRange, symSet->first, symSet->second);
     }
     
-    return NULL;
+    // Create the result
+    remapped_symbol_map* newSet = new remapped_symbol_map();
+    
+    // Create a new set for each range we got in the previous step
+    for (sets_for_range::iterator it = setsContainingRange.begin(); it != setsContainingRange.end(); it++) {
+        newSet->identifier_for_symbols(it->first, it->second);
+    }
+    
+    // This is the result
+    return newSet;
 }
--- Dfa/remapped_symbol_map.h
+++ Dfa/remapped_symbol_map.h
@@ -17,9 +17,13 @@
 namespace dfa {
     /// \brief Class that represents a symbol map that has been remapped from another symbol map
     class remapped_symbol_map : public symbol_map {
-    private:
+    public:
         /// \brief Represents the set of new symbol sets that an old symbol set was remapped to
         typedef std::set<int> new_symbols;
+
+    private:
+        /// \brief Constant value indicating 'no symbols'
+        static const new_symbols s_NoSymbols;
         
         /// \brief Maps IDs in the old symbol set to IDs in this map
         typedef std::map<int, new_symbols> old_to_new_map;
@@ -33,6 +37,16 @@ namespace dfa {
         
         /// \brief Copy constructor
         remapped_symbol_map(const remapped_symbol_map& copyFrom);
+        
+        /// \brief Finds or adds an identifier for the specified symbol, adding the specified set of new symbols as the new symbols for this set
+        int identifier_for_symbols(const symbol_set& symbols, const new_symbols& newSymbols);
+        
+        /// \brief Given a symbol set ID in this set, returns the old sets that contained it
+        inline const new_symbols& old_symbols(int newSetId) const { 
+            old_to_new_map::const_iterator found = m_OldToNew.find(newSetId);
+            if (found != m_OldToNew.end()) return found->second;
+            return s_NoSymbols;
+        }
         
         /// \brief Factory method that generates a remapped symbol map by removing duplicates
         ///
