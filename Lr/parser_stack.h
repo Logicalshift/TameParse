a5248860ad4274ca2ab4d11468926abd3497c8c0
Andrew Hunter <andrew@logicalshift.co.uk>
2011-05-12 19:55:17 +0000

    Wrangled this so that the exterior parser_stack class is what will be used to access the stack (the actual stack implementation is inside, which may be confusing for maintainers, but makes more sense if you actually want to use the thing)
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%40930

--- Lr/parser_stack.h
+++ Lr/parser_stack.h
@@ -45,10 +45,14 @@ namespace lr {
     template<typename item_type, int initial_depth = 64> class parser_stack {
     public:
         class entry;
-        class reference;
         
-        friend class reference;
+    private:
+        class internal_stack;
         
+        friend class internal_stack;
+
+        typedef parser_stack<item_type, initial_depth> pstack;
+
     public:
         ///
         /// \brief Structure representing an entry in a parser stack
@@ -56,7 +60,7 @@ namespace lr {
         class entry {
         private:
             friend class parser_stack<item_type, initial_depth>;
-            friend class parser_stack<item_type, initial_depth>::reference;
+            friend class parser_stack<item_type, initial_depth>::internal_stack;
             
             /// \brief -1 for a 'head' element, -2 for an empty element, or the entry 'below' this one
             int m_PreviousIndex;
@@ -77,249 +81,229 @@ namespace lr {
         };
         
     private:
-        /// \brief The first stack reference known about by this stack
-        reference* m_RootReference;
-        
-        /// \brief A vector of entries in this stack
-        std::vector<entry> m_Stack;
-        
-        /// \brief The first unused stack entry
-        int m_FirstUnused;
-        
-        /// \brief The number of free entries in the stack
-        int m_NumFree;
-        
-        /// \brief Creates a new stack
-        parser_stack()
-        : m_RootReference(NULL) {
-            m_Stack.resize(initial_depth);
-            m_FirstUnused = 0;
-        }
-        
-    public:
-        /// \brief Destroys the stack (and any remaining references)
-        ~parser_stack() {
-            // Delete any remaining references
-            while (m_RootReference != NULL) {
-                delete m_RootReference;
+        class internal_stack {
+            /// \brief The first stack reference known about by this stack
+            parser_stack<item_type, initial_depth>* m_RootReference;
+            
+            /// \brief A vector of entries in this stack
+            std::vector<entry> m_Stack;
+            
+            /// \brief The first unused stack entry
+            int m_FirstUnused;
+            
+            /// \brief The number of free entries in the stack
+            int m_NumFree;
+            
+        public:
+            /// \brief Creates a new stack
+            internal_stack()
+            : m_RootReference(NULL) {
+                m_Stack.resize(initial_depth);
+                m_FirstUnused = 0;
             }
-        }
-        
-        /// \brief Garbage collects this stack
-        void collect() {
-            // Create a vector of marked elements
-            std::vector<bool> marks;
-            marks.resize(m_Stack.size(), false);
-            
-            // Mark any entries used by an active reference
-            std::stack<int> waiting;
-            
-            reference* ref = m_RootReference;
-            while (ref != NULL) {
-                waiting.push(ref->m_Index);
-                ref = ref->m_Next;
+
+            /// \brief Destroys the stack (and any remaining references)
+            ~parser_stack() {
+                // Delete any remaining references
+                while (m_RootReference != NULL) {
+                    delete m_RootReference;
+                }
             }
             
-            // Mark any items that are in use
-            while (!waiting.empty()) {
-                // Get the next item to process
-                int next = waiting.top();
-                waiting.pop();
+            /// \brief Garbage collects this stack
+            void collect() {
+                // Create a vector of marked elements
+                std::vector<bool> marks;
+                marks.resize(m_Stack.size(), false);
                 
-                // Mark this item as being in-use
-                marks[next] = true;
+                // Mark any entries used by an active reference
+                std::stack<int> waiting;
                 
-                // Push the preceeding entry if there is one
-                entry& waitingEntry = m_Stack[next];
-                if (waitingEntry.m_PreviousIndex >= 0) {
-                    if (!marks[waitingEntry.m_PreviousIndex]) {
-                        waiting.push(waitingEntry.m_PreviousIndex);
+                pstack* ref = m_RootReference;
+                while (ref != NULL) {
+                    waiting.push(ref->m_Index);
+                    ref = ref->m_Next;
+                }
+                
+                // Mark any items that are in use
+                while (!waiting.empty()) {
+                    // Get the next item to process
+                    int next = waiting.top();
+                    waiting.pop();
+                    
+                    // Mark this item as being in-use
+                    marks[next] = true;
+                    
+                    // Push the preceeding entry if there is one
+                    entry& waitingEntry = m_Stack[next];
+                    if (waitingEntry.m_PreviousIndex >= 0) {
+                        if (!marks[waitingEntry.m_PreviousIndex]) {
+                            waiting.push(waitingEntry.m_PreviousIndex);
+                        }
+                    }
+                }
+                
+                // Sweep any unused items in the stack
+                for (int x=0; x<m_Stack.size(); x++) {
+                    if (!marks[x]) {
+                        m_Stack[x].m_PreviousIndex = entry::empty;
                     }
                 }
             }
             
-            // Sweep any unused items in the stack
-            for (int x=0; x<m_Stack.size(); x++) {
-                if (!marks[x]) {
-                    m_Stack[x].m_PreviousIndex = entry::empty;
-                }
-            }
-        }
-        
-    private:
-        void grow_stack() {
-            // Work out how many new items to create
-            int numNew = (int)m_Stack.size();
-            if (numNew > initial_depth * 8) numNew = initial_depth * 8;
-            
-            // Resize the stack by this amount
-            m_Stack.resize(m_Stack.size() + numNew);
-            m_NumFree += numNew;
-        }
-        
-        /// \brief Finds the next unused item
-        inline int get_new() {
-            // Collect if we've run out of free items, and grow the stack if it's looking empty
-            if (m_NumFree <= 0) {
-                collect();
-                if (m_NumFree < initial_depth/4) {
-                    grow_stack();
-                }
+        private:
+            void grow_stack() {
+                // Work out how many new items to create
+                int numNew = (int)m_Stack.size();
+                if (numNew > initial_depth * 8) numNew = initial_depth * 8;
+                
+                // Resize the stack by this amount
+                m_Stack.resize(m_Stack.size() + numNew);
+                m_NumFree += numNew;
             }
             
-            // Find a free entry
-            while (m_Stack[m_FirstUnused].m_PreviousIndex != entry::empty) {
+            /// \brief Finds the next unused item
+            inline int get_new() {
+                // Collect if we've run out of free items, and grow the stack if it's looking empty
+                if (m_NumFree <= 0) {
+                    collect();
+                    if (m_NumFree < initial_depth/4) {
+                        grow_stack();
+                    }
+                }
+                
+                // Find a free entry
+                while (m_Stack[m_FirstUnused].m_PreviousIndex != entry::empty) {
+                    m_FirstUnused++;
+                    if (m_FirstUnused >= m_Stack.size()) m_FirstUnused = 0;
+                }
+                
+                // m_FirstUnused now points to an entry we can use
+                int result = m_FirstUnused;
                 m_FirstUnused++;
-                if (m_FirstUnused >= m_Stack.size()) m_FirstUnused = 0;
-            }
-            
-            // m_FirstUnused now points to an entry we can use
-            int result = m_FirstUnused;
-            m_FirstUnused++;
-            
-            // Make this a 'head' entry
-            m_Stack[result].m_PreviousIndex = entry::head;
-            
-            return m_FirstUnused;
-        }
-        
-    public:
-        ///
-        /// \brief Class representing a reference to an entry in a parser stack
-        ///
-        /// Typically, a single reference is used to point at the head of the stack. Note that all references
-        /// must be destroyed before a stack is released.
-        ///
-        /// As the stacks are garbage collected, you can create a 'new' stack state simply by duplicating a
-        /// reference.
-        ///
-        class reference {
-        private:
-            friend class parser_stack<item_type, initial_depth>;
-            
-            /// \brief The stack that owns this reference
-            parser_stack<item_type, initial_depth>* m_Stack;
-            
-            /// \brief The index of this reference
-            int m_Index;
-            
-            /// \brief The previous reference in the list of references
-            reference* m_Last;
-            
-            /// \brief The next reference in the list of references
-            reference* m_Next;
-            
-        private:
-            reference(parser_stack<item_type, initial_depth>* stack, int index)
-            : m_Index(index)
-            , m_Stack(stack) {
-                m_Next = m_Stack->m_RootReference;
-                m_Last = NULL;
-                m_Stack->m_RootReference = this;
-            }
-            
-        public:
-            reference() 
-            : m_Stack(new parser_stack<item_type, initial_depth>)
-            , m_Index(m_Stack->get_unused()) {
-                m_Next = m_Stack->m_RootReference;
-                m_Last = NULL;
-                m_Stack->m_RootReference = this;
-            }
-            
-            /// \brief Creates a copy of a particular reference
-            inline reference(const reference& copyFrom)
-            : m_Stack(copyFrom.m_Stack)
-            , m_Index(copyFrom.m_Index) {
-                m_Next = m_Stack->m_RootReference;
-                m_Last = NULL;
-                m_Stack->m_RootReference = this;
-            }
-            
-            /// \brief Assignment operator
-            inline reference& operator=(const reference& copyFrom) {
-                // This will only work for references from the same stack
-                m_Index = copyFrom.m_Index;
                 
-                return *this;
-            }
-            
-            /// \brief Destructor
-            inline ~reference() {
-                // Remove this reference from the list of references
-                if (m_Next) {
-                    m_Next->m_Last = m_Last;
-                }
-                if (m_Last) {
-                    m_Last->m_Next = m_Next;
-                } else {
-                    m_Stack->m_RootReference = m_Next;
-                }
+                // Make this a 'head' entry
+                m_Stack[result].m_PreviousIndex = entry::head;
                 
-                if (m_Stack->m_RootReference == NULL) {
-                    delete m_Stack;
-                }
-            }
-            
-            inline entry& operator*() {
-                return m_Stack->m_Stack[m_Index];
-            }
-            
-            inline entry* operator->() {
-                return &m_Stack->m_Stack[m_Index];
-            }
-
-            inline const entry& operator*() const {
-                return m_Stack->m_Stack[m_Index];
-            }
-            
-            inline const entry* operator->() const {
-                return &m_Stack->m_Stack[m_Index];
-            }
-            
-            /// \brief Returns the entry at the specified offset from this entry. 'x' should be a negative value
-            ///
-            /// IE, reference[-1] gives the entry preceeding this one on the stack
-            inline entry& operator[](int x) {
-                int index = m_Index;
-                for (int pos = x; pos < 0; pos++) {
-                    int nextIndex = m_Stack[index].m_PreviousIndex;
-                    if (nextIndex >= 0) index = nextIndex;
-                }
-                return m_Stack[index];
-            }
-
-            /// \brief Pushes a new item onto the stack, and updates this to point at it
-            inline void push(int state, const item_type& newItem) {
-                int newIndex = m_Stack->get_new();
-                
-                entry& newEntry = m_Stack->m_Stack[newIndex];
-                
-                newEntry.state              = state;
-                newEntry.item               = newItem;
-                newEntry.m_PreviousIndex    = m_Index;
-                
-                m_Index = newIndex;
-            }
-            
-            /// \brief Pops an item from the stack (returns false if this is currently pointing at a head item)
-            ///
-            /// This reference is adjusted to point at the new head of the stack
-            inline bool pop() {
-                entry& ourEntry = operator*();
-                if (ourEntry.m_PreviousIndex == -1) return false;
-                m_Index = ourEntry.m_PreviousIndex;
-                return true;
+                return m_FirstUnused;
             }
         };
         
+    private:
+        
+        /// \brief The stack that owns this reference
+        internal_stack* m_Stack;
+        
+        /// \brief The index of this reference
+        int m_Index;
+        
+        /// \brief The previous reference in the list of references
+        pstack* m_Last;
+        
+        /// \brief The next reference in the list of references
+        pstack* m_Next;
+        
+    private:
+        parser_stack(pstack* stack, int index)
+        : m_Index(index)
+        , m_Stack(stack) {
+            m_Next = m_Stack->m_RootReference;
+            m_Last = NULL;
+            m_Stack->m_RootReference = this;
+        }
+        
     public:
-        /// \brief Gets a reference to an initial stack item (which can be the head of a new parser stack)
-        inline reference get_head() {
-            int index = get_new();
-            m_Stack[index].state    = 0;
-            m_Stack[index].item     = item_type();
-            return reference(this, index);
+        parser_stack() 
+        : m_Stack(new parser_stack<item_type, initial_depth>)
+        , m_Index(m_Stack->get_unused()) {
+            m_Next = m_Stack->m_RootReference;
+            m_Last = NULL;
+            m_Stack->m_RootReference = this;
+        }
+        
+        /// \brief Creates a copy of a particular reference
+        inline parser_stack(const parser_stack& copyFrom)
+        : m_Stack(copyFrom.m_Stack)
+        , m_Index(copyFrom.m_Index) {
+            m_Next = m_Stack->m_RootReference;
+            m_Last = NULL;
+            m_Stack->m_RootReference = this;
+        }
+        
+        /// \brief Assignment operator
+        inline parser_stack& operator=(const parser_stack& copyFrom) {
+            // This will only work for references from the same stack
+            m_Index = copyFrom.m_Index;
+            
+            return *this;
+        }
+        
+        /// \brief Destructor
+        inline ~parser_stack() {
+            // Remove this reference from the list of references
+            if (m_Next) {
+                m_Next->m_Last = m_Last;
+            }
+            if (m_Last) {
+                m_Last->m_Next = m_Next;
+            } else {
+                m_Stack->m_RootReference = m_Next;
+            }
+            
+            if (m_Stack->m_RootReference == NULL) {
+                delete m_Stack;
+            }
+        }
+        
+        inline entry& operator*() {
+            return m_Stack->m_Stack[m_Index];
+        }
+        
+        inline entry* operator->() {
+            return &m_Stack->m_Stack[m_Index];
+        }
+
+        inline const entry& operator*() const {
+            return m_Stack->m_Stack[m_Index];
+        }
+        
+        inline const entry* operator->() const {
+            return &m_Stack->m_Stack[m_Index];
+        }
+        
+        /// \brief Returns the entry at the specified offset from this entry. 'x' should be a negative value
+        ///
+        /// IE, reference[-1] gives the entry preceeding this one on the stack
+        inline entry& operator[](int x) {
+            int index = m_Index;
+            for (int pos = x; pos < 0; pos++) {
+                int nextIndex = m_Stack[index].m_PreviousIndex;
+                if (nextIndex >= 0) index = nextIndex;
+            }
+            return m_Stack[index];
+        }
+
+        /// \brief Pushes a new item onto the stack, and updates this to point at it
+        inline void push(int state, const item_type& newItem) {
+            int newIndex = m_Stack->get_new();
+            
+            entry& newEntry = m_Stack->m_Stack[newIndex];
+            
+            newEntry.state              = state;
+            newEntry.item               = newItem;
+            newEntry.m_PreviousIndex    = m_Index;
+            
+            m_Index = newIndex;
+        }
+        
+        /// \brief Pops an item from the stack (returns false if this is currently pointing at a head item)
+        ///
+        /// This reference is adjusted to point at the new head of the stack
+        inline bool pop() {
+            entry& ourEntry = operator*();
+            if (ourEntry.m_PreviousIndex == -1) return false;
+            m_Index = ourEntry.m_PreviousIndex;
+            return true;
         }
     };
 }
