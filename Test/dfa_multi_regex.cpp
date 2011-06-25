5cb4c40b8681f8f14dc73752442d743d32873e4a
Andrew Hunter <andrew@logicalshift.co.uk>
2011-06-25 20:01:42 +0000

    Added some tests to multi-regex that manage to demo some of the issues that exist, and also that the 'less general' character sets seem to fix the problem
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401044

--- Test/dfa_multi_regex.cpp
+++ Test/dfa_multi_regex.cpp
@@ -62,7 +62,16 @@ void test_dfa_multi_regex::run_tests() {
     test("a-or-b5", "(a|b)", "(a|b)a", "a", "ba");
     test("a-or-b6", "(a|b)", "(a|b)a", "b", "ba");
 
+    test("not-a-or-a", "[^a]", "a", "b", "a");
+    test("anything-or-a", "a", ".", "a", "b");
+
     test("language-or-identifier1", "language", "[A-Za-z\\-][A-Za-z\\-0-9]*", "language", "some-identifier");
     test("language-or-identifier2", "language", "[A-Za-z\\-][A-Za-z\\-0-9]*", "language", "lang-identifier");
     test("language-or-identifier3", "language", "[A-Za-z\\-][A-Za-z\\-0-9]*", "language", "language2");
+    
+    test("character-or-identifier", "'(.|(\\\\.))'", "[A-Za-z\\-][A-Za-z\\-0-9]*", "'a'", "some-identifier");
+    test("regex-or-character1", "/([^/]|(\\\\/))*/", "'(.|(\\\\.))'", "/re/", "'a'");
+    test("regex-or-character2", "/([a-z]|(\\\\/))*/", "'([a-z]|(\\\\[a-z]))'", "/re/", "'a'");
+    test("regex-or-identifier1", "/([^/]|(\\\\/))*/", "[A-Za-z\\-][A-Za-z\\-0-9]*", "/re/", "some-identifier");
+    test("regex-or-identifier2", "/([a-z]|(\\\\/))*/", "[A-Za-z\\-][A-Za-z\\-0-9]*", "/re/", "some-identifier");
 }
