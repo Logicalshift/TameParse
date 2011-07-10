9408aa4c1f6a010512db35124514dc81ec4b3a84
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-10 10:21:43 +0000

    Seeing as I'm trying to debug the parser, added some tracing which shows the state and the contents of the lookahead (reveals the current failure with dealing with closures, I think: see state #35 of the parser - is missing some important items from the closure for some reason)
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401100

--- Lr/parser.h
+++ Lr/parser.h
@@ -724,6 +724,7 @@ namespace lr {
                 
                 // Get the state
                 int state = m_Stack->state;
+                std::wcerr << state << L" " << (la.item()?la->content<wchar_t>():L"$") << std::endl;
                 
                 // Get the action for this lookahead
                 int sym;
