7de418ad175d83d5fd111abcdb60a5f8b473af38
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-10 10:36:07 +0000

    Fixed dumb issue with operator< when applied to rules - was only testing the nonterminal for less than and assuming that nonterminals which were greater than were the same :-/
    Not sure why I made this same mistake repeatedly :-(
    
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401101

--- ContextFree/rule.cpp
+++ ContextFree/rule.cpp
@@ -72,6 +72,7 @@ bool rule::operator<(const rule& compareTo) const {
     
     // The nonterminal should be reasonably fast to compare
     if (m_NonTerminal < compareTo.m_NonTerminal)    return true;
+    if (m_NonTerminal > compareTo.m_NonTerminal)    return false;
     
     // Compare the items
     return compare_items(compareTo) < 0;
