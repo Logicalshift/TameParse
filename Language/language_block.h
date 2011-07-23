681441cb160253ee472ac6e8a083eca34ffe587f
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-23 16:31:54 +0000

    Started work on some structures for representing a language block and its content
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401130

--- Language/language_block.h
+++ Language/language_block.h
@@ -9,14 +9,33 @@
 #ifndef _LANGUAGE_LANGUAGE_BLOCK_H
 #define _LANGUAGE_LANGUAGE_BLOCK_H
 
+#include <string>
+#include <vector>
+
 #include "Language/block.h"
+#include "Language/language_unit.h"
 
 namespace language {
     ///
     /// \brief Class representing the content of a language block from a parser definition file
     ///
     class language_block : public block {
+    public:
+        /// \brief List of identifiers
+        typedef std::vector<std::wstring> identifier_list;
+        
+        /// \brief List of language units
+        typedef std::vector<language_unit*> unit_list;
+        
     private:
+        /// \brief The identifier for this language block
+        std::wstring m_Identifier;
+        
+        /// \brief A list of the identifiers of the languages that this language should inherit from
+        identifier_list m_Inherits;
+        
+        /// \brief A list of the language units that define this language
+        unit_list m_Units;
         
     public:
     };
--- Language/language_unit.h
+++ Language/language_unit.h
@@ -5,3 +5,15 @@
 //  Created by Andrew Hunter on 17/07/2011.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
+
+#ifndef _LANGUAGE_LANGUAGE_UNIT_H
+#define _LANGUAGE_LANGUAGE_UNIT_H
+
+namespace language {
+    /// \brief Class that represents an item in a language definition
+    class language_unit {
+        
+    };
+}
+
+#endif
