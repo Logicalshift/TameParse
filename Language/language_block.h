6493bb045991b70be2e3d4b7ff606be6cece5430
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-24 14:29:26 +0000

    Defined a language block
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401134

--- Language/language_block.h
+++ Language/language_block.h
@@ -27,6 +27,9 @@ namespace language {
         /// \brief List of language units
         typedef std::vector<language_unit*> unit_list;
         
+        /// \brief Iterator that retrieves the contents of this language block
+        typedef unit_list::const_iterator iterator;
+        
     private:
         /// \brief The identifier for this language block
         std::wstring m_Identifier;
@@ -38,6 +41,35 @@ namespace language {
         unit_list m_Units;
         
     public:
+        /// \brief Creates a new empty language block with the specified identifier
+        language_block(const std::wstring& identifier, position start, position end);
+        
+        /// \brief Creates a copy of this language block
+        language_block(const language_block& copyFrom);
+        
+        /// \brief Copies a language block into this object
+        language_block& operator=(const language_block& copyFrom);
+        
+        /// \brief Destructor
+        virtual ~language_block();
+        
+        /// \brief Adds the identifier of a language that this language inherits from
+        void add_inherits(const std::wstring& identifier);
+        
+        /// \brief Adds a language unit to this object (the unit becomes owned by this object, which will destroy it when it is itself destroyed)
+        void add_unit(language_unit* unit);
+        
+        /// \brief The identifiers that this language inherits from
+        const identifier_list& inherits() const { return m_Inherits; }
+        
+        /// \brief The identifier for this language
+        const std::wstring& identifier() const { return m_Identifier; }
+        
+        /// \brief The first language unit in this block
+        iterator begin() const { return m_Units.begin(); }
+        
+        /// \brief The item after the final language unit in this block
+        iterator end() const { return m_Units.end(); }
     };
 }
 
