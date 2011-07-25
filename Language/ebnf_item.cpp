93a618175f8b95645c594c6e1cb63e59cc301773
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-25 19:57:40 +0000

    Finished the implementation of the EBNF item class
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401148

--- Language/ebnf_item.cpp
+++ Language/ebnf_item.cpp
@@ -33,14 +33,37 @@ ebnf_item::ebnf_item(const ebnf_item& copyFrom) {
 /// \brief Destructor
 ebnf_item::~ebnf_item() {
     // Clear out the child items
+    for (ebnf_item_list::iterator toDelete = m_ChildItems.begin(); toDelete != m_ChildItems.end(); toDelete++) {
+        delete *toDelete;
+    }
+    m_ChildItems.clear();
 }
 
 /// \brief Fills the content of this item with the content of the specified item
 ebnf_item& ebnf_item::operator=(const ebnf_item& copyFrom) {
     // Clear out the child items
+    for (ebnf_item_list::iterator toDelete = m_ChildItems.begin(); toDelete != m_ChildItems.end(); toDelete++) {
+        delete *toDelete;
+    }
+    m_ChildItems.clear();
+    
+    // Copy the items from the source
+    m_Identifier        = copyFrom.m_Identifier;
+    m_SourceIdentifier  = copyFrom.m_SourceIdentifier;
+    
+    for (iterator toCopy = copyFrom.begin(); toCopy != copyFrom.end(); toCopy++) {
+        m_ChildItems.push_back(new ebnf_item(**toCopy));
+    }
+    
+    set_start_pos(copyFrom.start_pos());
+    set_end_pos(copyFrom.end_pos());
+    
+    return *this;
 }
 
 /// \brief Adds a child item to this item
 ///
 /// This item will delete the item when it has finished with it.
-void add_child(ebnf_item* newChild);
+void ebnf_item::add_child(ebnf_item* newChild) {
+    m_ChildItems.push_back(newChild);
+}
