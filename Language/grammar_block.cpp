e0156629da79509a595e1df8cf3a270cbbf1678d
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-26 21:00:27 +0000

    Added the implementation of a grammar block, which should finish the language definition for now.
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401151

--- Language/grammar_block.cpp
+++ Language/grammar_block.cpp
@@ -7,3 +7,45 @@
 //
 
 #include "grammar_block.h"
+
+using namespace language;
+
+/// \brief Creates a new grammar block
+grammar_block::grammar_block(position start, position end)
+: block(start, end) {
+}
+
+/// \brief Creates a grammar block by copying an existing block
+grammar_block::grammar_block(grammar_block& copyFrom) {
+    (*this) = copyFrom;
+}
+
+/// \brief Destructor
+grammar_block::~grammar_block() {
+    // Delete the nonterminals
+    for (nonterminal_definition_list::iterator toDelete = m_Nonterminals.begin(); toDelete != m_Nonterminals.end(); toDelete++) {
+        delete *toDelete;
+    }
+    m_Nonterminals.clear();
+}
+
+/// \brief Assigns the value of this grammar block
+grammar_block& grammar_block::operator=(const grammar_block& copyFrom) {
+    // Delete the nonterminals
+    for (nonterminal_definition_list::iterator toDelete = m_Nonterminals.begin(); toDelete != m_Nonterminals.end(); toDelete++) {
+        delete *toDelete;
+    }
+    m_Nonterminals.clear();
+    
+    // Copy the nonterminals from the source
+    for (iterator toCopy = copyFrom.begin(); toCopy != copyFrom.end(); toCopy++) {
+        m_Nonterminals.push_back(new nonterminal_definition(**toCopy));
+    }
+    
+    return *this;
+}
+
+/// \brief Adds a new nonterminal to this object, which will become responsible for freeing it
+void grammar_block::add_nonterminal(nonterminal_definition* newNonterminal) {
+    m_Nonterminals.push_back(newNonterminal);
+}
