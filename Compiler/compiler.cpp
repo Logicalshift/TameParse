cda16e75ff989ddbc6650de6a556878b52973666
Andrew Hunter <andrew@logicalshift.co.uk>
2011-07-30 21:35:35 +0000

    Added an empty compiler class, which will be used to generate the output files
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401172

---
+++ Compiler/compiler.cpp
@@ -0,0 +1,7 @@
+//
+//  compiler.cpp
+//  Parse
+//
+//  Created by Andrew Hunter on 30/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
---
+++ Compiler/compiler.h
@@ -0,0 +1,23 @@
+//
+//  compiler.h
+//  Parse
+//
+//  Created by Andrew Hunter on 30/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _COMPILER_COMPILER_H
+#define _COMPILER_COMPILER_H
+
+#include "Language/definition_file.h"
+
+namespace compiler {
+    ///
+    /// \brief Class that handles compiling a definition file into a parser in a particular language
+    ///
+    class compiler {
+        
+    };
+}
+
+#endif
