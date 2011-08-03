2ade3b705c3e20a0080102471d0e36ad9b70f08c
Andrew Hunter <andrew@logicalshift.co.uk>
2011-08-03 21:05:20 +0000

    Hm, probably actually want to start with something that just handles a language block with no inheritance, as this is the simplest form of compilation that will be useful.
    Need to think of a way to deal with more complex languages with inheritance, though (however, I think there's also a need to deal with the way that the parsers and particularly the lexers deal with this)
    
    --HG--
    branch : Parse
    rename : Compiler/compiler.h => Compiler/language_compiler.h
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401174

--- Compiler/language_compiler.cpp
+++ Compiler/language_compiler.cpp
@@ -1,7 +1,9 @@
 //
-//  compiler.cpp
+//  language_compiler.cpp
 //  Parse
 //
 //  Created by Andrew Hunter on 30/07/2011.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
+
+#include "Compiler/language_compiler.h"
---
+++ Compiler/language_compiler.h
@@ -0,0 +1,31 @@
+//
+//  compiler.h
+//  Parse
+//
+//  Created by Andrew Hunter on 30/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _COMPILER_COMPILER_H
+#define _COMPILER_COMPILER_H
+
+#include "Language/language_block.h"
+
+namespace compiler {
+    ///
+    /// \brief Class that handles compiling a language block into a lexer and a grammar
+    ///
+    /// This class handles the steps necessary to compile a language block with no inheritance into a grammar and a lexer.
+    /// These can in turn be used to build a finished parser.
+    ///
+    class language_compiler {
+    private:
+        /// \brief The language block that this will compile
+        language::language_block* m_Language;
+    };
+    
+    // TODO: need to add virtual methods to make it possible to write a subclass that can deal with more complex languages 
+    // with inheritance
+}
+
+#endif
