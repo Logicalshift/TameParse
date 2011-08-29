b3e7139e4bb57deda114ae6deaf131a8f5cda878
Andrew Hunter <andrew@logicalshift.co.uk>
2011-08-29 15:20:47 +0000

    Should now have the actions necessary to write out the symbols and the DFA for an output stage
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401228

--- Compiler/output_stage.cpp
+++ Compiler/output_stage.cpp
@@ -7,3 +7,235 @@
 //
 
 #include "output_stage.h"
+
+using namespace std;
+using namespace dfa;
+using namespace compiler;
+
+/// \brief Creates a new output stage
+output_stage::output_stage(console_container& console, const std::wstring& filename, lexer_stage* lexer, language_stage* language, lr_parser_stage* parser)
+: compilation_stage(console, filename)
+, m_LexerStage(lexer)
+, m_LanguageStage(language)
+, m_ParserStage(parser) {
+}
+
+/// \brief Destructor
+output_stage::~output_stage() {
+}
+
+/// \brief Compiles the parser specified by this stage
+///
+/// Subclasses can override this if they want to substantially change the way that the
+/// compiler is generated.
+void output_stage::compile() {
+	// TODO: sanity check
+
+	// Start writing the output
+	begin_output();
+
+	// Write out the symbol IDs, etc first
+	define_symbols();
+
+	// Next, write out the lexer definition
+	define_lexer_tables();
+
+	// Finished writing the output
+	end_output();
+}
+
+/// \brief Defines the symbols associated with this language
+void output_stage::define_symbols() {
+	// TODO: sanity check
+
+	// Write out the terminal symbols that are defined in this language
+	begin_terminal_symbols();
+
+	for (int symbolId = 0; symbolId < m_LanguageStage->terminals()->count_symbols(); symbolId++) {
+		terminal_symbol(m_LanguageStage->terminals()->name_for_symbol(symbolId), symbolId);
+	}
+
+	end_terminal_symbols();
+
+	// Write out the nonterminal symbols that are defined in this language
+	begin_nonterminal_symbols();
+
+	for (int symbolId = 0; symbolId < m_LanguageStage->grammar()->max_nonterminal(); symbolId++) {
+		nonterminal_symbol(m_LanguageStage->grammar()->name_for_nonterminal(symbolId), symbolId);
+	}
+
+	end_nonterminal_symbols();
+}
+
+/// \brief Writes out the lexer tables (the symbol map and the state table)
+void output_stage::define_lexer_tables() {
+	// TODO: sanity check
+
+	// Get the lexer DFA
+	const ndfa* dfa = m_LexerStage->dfa();
+
+	// Get the symbol table
+	const class symbol_map& symbols = dfa->symbols();
+
+	// Write to the result
+	begin_lexer_symbol_map(symbols.count_sets());
+
+	// Go through all of the symbol sets
+	for (symbol_map::iterator setIt = symbols.begin(); setIt != symbols.end(); setIt++) {
+		// Go through the ranges in each set
+		for (symbol_set::iterator rangeIt = setIt->first->begin(); rangeIt != setIt->first->end(); rangeIt++) {
+			symbol_map(*rangeIt, setIt->second);
+		}
+	}
+
+	end_lexer_symbol_map();
+
+	// Start writing out the DFA
+	begin_lexer_state_machine(dfa->count_states());
+
+	// Write out each state in turn
+	for (int stateId = 0; stateId < dfa->count_states(); stateId++) {
+		// Start writing out this state
+		const state& state = dfa->get_state(stateId);
+		begin_lexer_state(stateId);
+
+		for (state::iterator transit = state.begin(); transit != state.end(); transit++) {
+			lexer_state_transition(transit->symbol_set(), transit->new_state());
+		}
+
+		end_lexer_state();
+	}
+
+	end_lexer_state_machine();
+
+	// Write out the accepting states
+	begin_lexer_accept_table();
+
+	for (int stateId = 0; stateId < dfa->count_states(); stateId++) {
+		// Get the actions for this state
+		typedef ndfa::accept_action_list accept_action_list;
+		const accept_action_list& actions = dfa->actions_for_state(stateId);
+
+		// Nothing to do if there are no actions for this state
+		if (actions.begin() == actions.end()) {
+			nonaccepting_state(stateId);
+			continue;
+		}
+
+		// Write out the highest action
+		accept_action_list::const_iterator thisAction = actions.begin();
+        const accept_action* highest = *thisAction;
+
+        thisAction++;
+        for (; thisAction != actions.end(); thisAction++) {
+            if ((*highest) < **thisAction) {
+                highest = *thisAction;
+            }
+        }
+
+        // Write out this action
+        accepting_state(stateId, highest->symbol());
+	}
+
+	end_lexer_accept_table();
+}
+
+/// \brief About to begin writing out output
+void output_stage::begin_output() {
+	// Do nothing in the default implementation
+}
+
+/// \brief Finishing writing out output
+void output_stage::end_output() {
+	// Do nothing in the default implementation
+}
+
+/// \brief The output stage is about to produce a list of terminal symbols
+void output_stage::begin_terminal_symbols() {
+	// Do nothing in the default implementation
+}
+
+/// \brief Specifies the identifier for the terminal symbol with a given name
+void output_stage::terminal_symbol(const std::wstring& name, int identifier) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Finished writing out the terminal symbols
+void output_stage::end_terminal_symbols() {
+	// Do nothing in the default implementation
+}
+
+/// \brief The output stage is about to produce a list of non-terminal symbols
+void output_stage::begin_nonterminal_symbols() {
+	// Do nothing in the default implementation
+}
+
+/// \brief Specifies the identifier for the non-terminal symbol with a given name
+void output_stage::nonterminal_symbol(const std::wstring& name, int identifier) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Finished writing out the terminal symbols
+void output_stage::end_nonterminal_symbols() {
+	// Do nothing in the default implementation
+}
+
+/// \brief Starting to write out the symbol map for the lexer
+void output_stage::begin_lexer_symbol_map(int maxSetId) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Specifies that a given range of symbols maps to a particular identifier
+void output_stage::symbol_map(const dfa::range<int>& symbolRange, int identifier) {
+	// Do nothing in the default implementation
+}
+		
+/// \brief Finishing writing out the symbol map for the lexer
+void output_stage::end_lexer_symbol_map() {
+	// Do nothing in the default implementation
+}
+
+/// \brief About to begin writing out the lexer tables
+void output_stage::begin_lexer_state_machine(int numStates) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Starting to write out the transitions for a given state
+void output_stage::begin_lexer_state(int stateId) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Adds a transition for the current state
+void output_stage::lexer_state_transition(int symbolSet, int newState) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Finishes writing out a lexer state
+void output_stage::end_lexer_state() {
+	// Do nothing in the default implementation
+}
+
+/// \brief Finished writing out the lexer table
+void output_stage::end_lexer_state_machine() {
+	// Do nothing in the default implementation
+}
+
+/// \brief About to write out the list of accepting states for a lexer
+void output_stage::begin_lexer_accept_table() {
+	// Do nothing in the default implementation
+}
+
+/// \brief The specified state is not an accepting state
+void output_stage::nonaccepting_state(int stateId) {
+	// Do nothing in the default implementation
+}
+
+/// \brief The specified state is an accepting state
+void output_stage::accepting_state(int stateId, int acceptSymbolId) {
+	// Do nothing in the default implementation
+}
+
+/// \brief Finished the lexer acceptance table
+void output_stage::end_lexer_accept_table() {
+	// Do nothing in the default implementation
+}
--- Compiler/output_stage.h
+++ Compiler/output_stage.h
@@ -9,8 +9,13 @@
 #ifndef _COMPILER_OUTPUT_STAGE_H
 #define _COMPILER_OUTPUT_STAGE_H
 
+#include <string>
+
+#include "Dfa/range.h"
+
 #include "Compiler/compilation_stage.h"
 #include "Compiler/language_stage.h"
+#include "Compiler/lr_parser_stage.h"
 #include "Compiler/lexer_stage.h"
 
 namespace compiler {
@@ -19,10 +24,18 @@ namespace compiler {
 	///
 	class output_stage : public compilation_stage {
 	private:
-		
+		/// \brief The lexer stage that should be compiled
+		lexer_stage* m_LexerStage;
+
+		/// \brief The language stage that should be compiled
+		language_stage* m_LanguageStage;
+
+		/// \brief The LR parser that should be compiled
+		lr_parser_stage* m_ParserStage;
+
 	public:
 		/// \brief Creates a new output stage
-		output_stage(console_container& console, const std::wstring& filename, lexer_stage* lexer, language_stage* language);
+		output_stage(console_container& console, const std::wstring& filename, lexer_stage* lexer, language_stage* language, lr_parser_stage* parser);
 
 		/// \brief Destructor
 		virtual ~output_stage();
@@ -31,9 +44,83 @@ namespace compiler {
 		///
 		/// Subclasses can override this if they want to substantially change the way that the
 		/// compiler is generated.
-		void compile();
+		virtual void compile();
 
 	protected:
+		// Higher-level stages of the process
+
+		/// \brief Defines the symbols associated with this language
+		virtual void define_symbols();
+
+		/// \brief Writes out the lexer tables (the symbol map and the state table)
+		virtual void define_lexer_tables();
+
+	protected:
+		// Functions that represent various steps of the output of a language.
+		// These are intended to make it easy to write out a file in the specified language.
+		// They do nothing in this base class (but they are not abstract)
+		//
+		// You can override compile() or the define_x methods to get more fine-grained control
+
+		/// \brief About to begin writing out output
+		virtual void begin_output();
+
+		/// \brief Finishing writing out output
+		virtual void end_output();
+
+		/// \brief The output stage is about to produce a list of terminal symbols
+		virtual void begin_terminal_symbols();
+
+		/// \brief Specifies the identifier for the terminal symbol with a given name
+		virtual void terminal_symbol(const std::wstring& name, int identifier);
+
+		/// \brief Finished writing out the terminal symbols
+		virtual void end_terminal_symbols();
+
+		/// \brief The output stage is about to produce a list of non-terminal symbols
+		virtual void begin_nonterminal_symbols();
+
+		/// \brief Specifies the identifier for the non-terminal symbol with a given name
+		virtual void nonterminal_symbol(const std::wstring& name, int identifier);
+
+		/// \brief Finished writing out the terminal symbols
+		virtual void end_nonterminal_symbols();
+
+		/// \brief Starting to write out the symbol map for the lexer
+		virtual void begin_lexer_symbol_map(int maxSetId);
+
+		/// \brief Specifies that a given range of symbols maps to a particular identifier
+		virtual void symbol_map(const dfa::range<int>& symbolRange, int identifier);
+		
+		/// \brief Finishing writing out the symbol map for the lexer
+		virtual void end_lexer_symbol_map();
+
+		/// \brief About to begin writing out the lexer tables
+		virtual void begin_lexer_state_machine(int numStates);
+
+		/// \brief Starting to write out the transitions for a given state
+		virtual void begin_lexer_state(int stateId);
+
+		/// \brief Adds a transition for the current state
+		virtual void lexer_state_transition(int symbolSet, int newState);
+
+		/// \brief Finishes writing out a lexer state
+		virtual void end_lexer_state();
+
+		/// \brief Finished writing out the lexer table
+		virtual void end_lexer_state_machine();
+
+		/// \brief About to write out the list of accepting states for a lexer
+		virtual void begin_lexer_accept_table();
+
+		/// \brief The specified state is not an accepting state
+		virtual void nonaccepting_state(int stateId);
+
+		/// \brief The specified state is an accepting state
+		virtual void accepting_state(int stateId, int acceptSymbolId);
+
+		/// \brief Finished the lexer acceptance table
+		virtual void end_lexer_accept_table();
 	};
 }
 
