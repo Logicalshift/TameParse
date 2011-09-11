1a67c41fa87df9f01e6e6f98c67f7370bb3572ef
Andrew Hunter <andrew@logicalshift.co.uk>
2011-09-11 15:59:52 +0000

    Added a unicode class to contain the unicode character ranges we just added.
    --HG--
    branch : Parse
    extra : convert_revision : svn%3A3666fe7e-a552-0410-bc3c-bd4e4c681ee0/trunk/Parse%401255

---
+++ Util/unicode.cpp
@@ -0,0 +1,15 @@
+//
+//  unicode.cpp
+//  TameParse
+//
+//  Created by Andrew Hunter on 11/09/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <cstdlib>
+#include "unicode.h"
+
+using namespace util;
+
+// The unicode_data.h file contains the static variable declarations
+#include "Unicode/unicode_data.h"
