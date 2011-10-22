bd9e17e1265135fa60c9e0a825de56574596beec
Andrew Hunter <andrew@logicalshift.co.uk>
2011-10-22 15:21:00 +0100

    Added the basic mechanics of the stage, with no actual test running being performed
    --HG--
    branch : Testing

--- TameParse/Compiler/test_stage.cpp
+++ TameParse/Compiler/test_stage.cpp
@@ -6,8 +6,13 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
-#include "TameParse/Compiler/test_stage.h"
+#include <sstream>
 
+#include "TameParse/Compiler/test_stage.h"
+#include "TameParse/Language/test_block.h"
+
+using namespace std;
+using namespace dfa;
 using namespace language;
 using namespace compiler;
 
@@ -20,5 +25,63 @@ test_stage::test_stage(console_container& console, const std::wstring& filename,
 
 /// \brief Performs the actions associated with this compilation stage
 void test_stage::compile() {
-	// TODO: implement me
+	// Sanity check
+	if (!m_Definition.item()) {
+		cons().report_error(error(error::sev_bug, filename(), L"BUG_MISSING_DEFINITION", L"Definition for testing stage not specified", position(-1, -1, -1)));
+		return;
+	}
+
+	if (!m_Import) {
+		cons().report_error(error(error::sev_bug, filename(), L"BUG_MISSING_IMPORT", L"Imports for testing stage not specified", position(-1, -1, -1)));
+		return;
+	}
+    
+    // Output the stage name
+    cons().verbose_stream() << L"  = Running tests" << endl;
+
+	// Iterate through the definitions
+	for (definition_file::iterator defn = m_Definition->begin(); defn != m_Definition->end(); defn++) {
+		// Retrieve the tests for this block
+		const test_block* tests = (*defn)->test();
+
+		// Ignore blocks that aren't test blocks
+		if (!tests) continue;
+
+		// Retrieve the language for this test
+		const language_block*	language 		= m_Import->language_with_name(tests->language());
+		wstring                 languageFile    = m_Import->file_with_language(tests->language());
+
+		// Error if the language could not be found
+		if (!language) {
+            wstringstream msg;
+            msg << L"Unable to find language '" << tests->language() << "'";
+			cons().report_error(error(error::sev_error, filename(), L"CANT_FIND_LANGUAGE", msg.str(), tests->start_pos()));
+			continue;
+		}
+        
+        // Message for these tests
+        wstringstream testMessages;
+        testMessages << endl << L"    Tests for " << tests->language() << endl;
+        
+        // Passed/failed ratios for these tests
+        int passed  = 0;
+        int failed  = 0;
+
+        // Run the tests themselves
+        
+        // Write the messages. We use the verbose stream if the tests mostly passed
+        if (failed) {
+            cons().message_stream() << testMessages.str();
+        } else {
+            cons().verbose_stream() << testMessages.str();
+        }
+        
+        // Final message
+        cons().message_stream() << L"    " << tests->language() << L": " << passed << L"/" << (passed+failed) << L" passed" << endl;
+        
+        // Warning if there are no tests for this language
+        if (passed == 0 && failed == 0) {
+            cons().report_error(error(error::sev_warning, filename(), L"NO_TESTS_TO_RUN", L"Found no tests to run", tests->start_pos()));
+        }
+	}
 }
