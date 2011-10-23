09fa38e83def311cee02bc2fb73f67e9bc07483c
Andrew Hunter <andrew@logicalshift.co.uk>
2011-10-23 21:15:20 +0100

    Will now report failure if any of the test scripts fail instead of just the last one
    --HG--
    branch : Examples

--- Examples/run-tests.sh
+++ Examples/run-tests.sh
@@ -9,7 +9,17 @@ definition_dir=${srcdir}
 tameparse=../parsetool/tameparse
 
 # Iterate through the items in the  directory
+success=1
+
 for definition_file in ${definition_dir}/*.tp
 do
 	${tameparse} --run-tests ${definition_file}
+	if [ "$?" -ne "0" ]; then
+		success=0
+	fi
 done
+
+# Return failure if any of the tests failed
+if [ "$success" -ne "1" ]; then
+	exit 1
+fi
