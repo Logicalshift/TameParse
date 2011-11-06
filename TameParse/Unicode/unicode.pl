5b53211c09f7f7b97868dd0c13290fc141acad9c
Andrew Hunter <andrew@logicalshift.co.uk>
2011-11-06 23:22:12 +0000

    Updated formatting
    --HG--
    branch : Lexer

--- TameParse/Unicode/unicode.pl
+++ TameParse/Unicode/unicode.pl
@@ -114,7 +114,7 @@ print "\n    { NULL, NULL }\n};\n";
 print "\nconst unicode::block* unicode::s_EndBlock = s_Blocks + $count;\n";
 
 # Generate the array of uppercase values
-print "\nconst int unicode::s_UppercaseMap[][3] = {\n";
+print "\nconst int unicode::s_UppercaseMap[][3] = {\n    ";
 $first = 1;
 foreach my $range (@$uppercase) {
 	if ($first == 0) {
