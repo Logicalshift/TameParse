2953e610ef9767b3be718846177fc385641428d5
Andrew Hunter <andrew@logicalshift.co.uk>
2011-11-19 18:33:05 +0000

    Added a common header for inclusion in all of the cpp files when we're compiling
    --HG--
    branch : Refactoring

---
+++ TameParse/common.h
@@ -0,0 +1,19 @@
+//
+//  common.h
+//  Parse
+//
+//  Created by Andrew Hunter on 19/11/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _TAMEPARSE_COMMON_H
+#define _TAMEPARSE_COMMON_H
+
+// Common definitions that are included by every TameParse .cpp file
+// (Not installed; these definitions are internal to the TameParse library)
+
+// Extern template definitions
+#include "TameParse/stl_templates.h"
+#include "TameParse/templates.h"
+
+#endif
