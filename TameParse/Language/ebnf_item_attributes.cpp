8cfaf54c17ad9d07992896b0e95d5ca95140ffb4
Andrew Hunter <andrew@logicalshift.co.uk>
2012-01-06 16:29:06 +0000

    Added a storage class to contain the attributes that can be associated with an EBNF item.
    --HG--
    branch : Parser

---
+++ TameParse/Language/ebnf_item_attributes.cpp
@@ -0,0 +1,9 @@
+//
+//  ebnf_item_attributes.cpp
+//  Parse
+//
+//  Created by Andrew Hunter on 06/01/2012.
+//  Copyright 2012 Andrew Hunter. All rights reserved.
+//
+
+#include "TameParse/Language/ebnf_item_attributes.h"
---
+++ TameParse/Language/ebnf_item_attributes.h
@@ -0,0 +1,45 @@
+//
+//  ebnf_item_attributes.h
+//  Parse
+//
+//  Created by Andrew Hunter on 06/01/2012.
+//  Copyright 2012 Andrew Hunter. All rights reserved.
+//
+
+#ifndef _LANGUAGE_EBNF_ITEM_ATTRIBUTES_H
+#define _LANGUAGE_EBNF_ITEM_ATTRIBUTES_H
+
+#include <vector>
+#include <string>
+
+namespace language {
+	///
+	/// \brief Class representing the attributes associated with an EBNF item in a rule
+	///
+	class ebnf_item_attributes {
+	public:
+		/// \brief The possible actions to take on an item when it is part of a shift/reduce conflict
+		enum conflict_action {
+			/// \brief No action specified (report a shift/reduce conflict as a warning)
+			conflict_notspecified;
+
+			/// \brief Shift the symbol
+			conflict_shift,
+
+			/// \brief Reduce the rule (even if it will result in a parser error)
+			conflict_reduce,
+
+			/// \brief Reduce the rule provided that the symbol will be subsequently shifted
+			conflict_weakreduce,
+		};
+
+	public:
+		/// \brief The name of this item
+		std::wstring name;
+
+		/// \brief The action to take when this item is part of a shift/reduce conflict
+		conflict_action conflict_action;
+	};
+}
+
+#endif
