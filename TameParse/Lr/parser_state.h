949ef66684724aebbd7486c2633fec193f0cb832
Andrew Hunter <andrew@logicalshift.co.uk>
2012-01-17 23:04:28 +0000

    Formatting change to make it easier to set breakpoints
    --HG--
    branch : Parser-Lr1

--- TameParse/Lr/parser_state.h
+++ TameParse/Lr/parser_state.h
@@ -406,7 +406,9 @@ namespace lr {
                     
                     // If we can reduce via this item, then the result is true
                     fake_reduce(act, weakPos, weakStack, underlyingStack);
-                    if (can_reduce<symbol_fetcher>(symbol, weakPos, weakStack, underlyingStack)) return true;
+                    if (can_reduce<symbol_fetcher>(symbol, weakPos, weakStack, underlyingStack)) {
+                        return true;
+                    }
                     
                     // If not, keep looking for a stronger action
                     ++act;
