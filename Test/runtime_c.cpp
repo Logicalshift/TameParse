9462614212c8a9551c2e80acd0a1bf1ab47391fe
Andrew Hunter <andrew@logicalshift.co.uk>
2012-07-13 16:26:05 +0100

    Added basic functions, test that tries to create a parser

--- Test/runtime_c.cpp
+++ Test/runtime_c.cpp
@@ -25,10 +25,79 @@
 //  IN THE SOFTWARE.
 //
 
+#include <iostream>
+#include <fstream>
+
 #include "runtime_c.h"
 
 #include "Runtimes/C/cparse.h"
 
-void test_runtime_c::run_tests() {
+using namespace std;
 
+/// Simple lexer test: two identifiers
+static const char* identifierTest = "identifier1 identifier2";
+
+/// Runtime data structure
+struct runtime_data {
+    runtime_data(const char* string)
+    : inputStream((const unsigned char*) string)
+    , length(strlen(string))
+    , pos(0) { }
+
+    const unsigned char*    inputStream;
+    int                     length;
+    int                     pos;
+};
+
+static int read_symbol(void* data) {
+    runtime_data* rtData = (runtime_data*) data;
+
+    // EOF if we're over the end
+    if (rtData->pos >= rtData->length) return tp_lex_eof;
+
+    return (int) rtData->inputStream[rtData->pos++];
+}
+
+static void free_ast(void* astData, void* userData) {
+
+}
+
+static void* shift(int terminalId, int* symbols, int numSymbols, void* userData) {
+    return NULL;
+}
+
+static void* reduce(int nonterminalId, int numSymbols, void** astData, int ruleId, void* userData) {
+    return NULL;
+}
+
+void test_runtime_c::run_tests() {
+    // Read in the ansic.tpdat file (which is expected in whichever directory we're running in)
+    ifstream ansiStream;
+
+    ansiStream.open("ansic.tpdat", ios::binary);
+    ansiStream.seekg(0, ios::end);
+    int length = ansiStream.tellg();
+    ansiStream.seekg(0, ios::beg);
+
+    char* ansiBinary = new char[length];
+    ansiStream.read(ansiBinary, length);
+    ansiStream.close();
+
+    // Create the functions structure
+    tp_parser_functions functions;
+
+    functions.size          = sizeof(tp_parser_functions);
+    functions.readSymbol    = read_symbol;
+    functions.freeAstNode   = free_ast;
+    functions.shift         = shift;
+    functions.reduce        = reduce;
+
+    // Try to create a parser from the stream
+    tp_parser cParser = tp_create_parser((tp_parser_data*) ansiBinary, &functions, (void*) new runtime_data(identifierTest));
+
+    report("CreateParser", cParser != NULL);
+    if (!cParser) return;
+
+    // Finish up
+    delete[] ansiBinary;
 }
